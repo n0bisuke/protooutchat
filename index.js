93bf244e8dabb51d08e115c57685ea03759fe546
n0bisuke <rsksound@gmail.com>
2022-05-02 00:27:01 +0900

    init

---
+++ index.js
@@ -0,0 +1,22 @@
+
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  try {
+    res.send({ name: "hoge" });
+  } catch (error) {
+    res.sendStatus(500);
+  }
+});
+
+//app.listen(process.env.PORT || 3000);
+app.listen({ port: 3000 }, () => {
+  console.log(`Server ready at http://localhost:3000`);
+});
+console.log('starts');
+module.exports = app;
+// export default app;
