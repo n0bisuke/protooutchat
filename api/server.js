11ee95a2b43299cb52a13a5dc03434685cbb4150
n0bisuke <rsksound@gmail.com>
2022-05-02 00:55:26 +0900

    init

--- api/server.js
+++ api/server.js
@@ -12,7 +12,7 @@ const config = {
 const app = express();
 
 app.use(express.static('public'));
-// app.get('/', (req, res) => res.send('Hello LINE BOT!(GET)')); //ブラウザ確認用(無くても問題ない)
+app.get('/', (req, res) => res.send('Hello LINE BOT!(GET)')); //ブラウザ確認用(無くても問題ない)
 app.post('/webhook', line.middleware(config), (req, res) => {
     console.log(req.body.events);
 
