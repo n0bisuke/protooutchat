1aa8d322064896e4bc5ad59aae214eaaa678bf31
n0bisuke <rsksound@gmail.com>
2022-05-02 00:53:37 +0900

    init

--- api/server.js
+++ api/server.js
@@ -42,7 +42,7 @@ async function handleEvent(event) {
 }
 
 app.listen(PORT);
-console.log(`Server running at ${PORT}`);
-
-// (process.env.NOW_REGION) ? module.exports = app : app.listen(PORT);
 // console.log(`Server running at ${PORT}`);
+
+(process.env.NOW_REGION) ? module.exports = app : app.listen(PORT);
+console.log(`Server running at ${PORT}`);
